a991db89002b328aa7ebefe3ff0be31815617665
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-10-29 21:38:42 +0500

    Solved the task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
-
+    setRange(a, b) {
+        this.min = a + 1;
+        this.max = b;
     }
 
     guess() {
-
+        this.middle = Math.floor((this.max + this.min) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle + 1;
     }
 }
 
